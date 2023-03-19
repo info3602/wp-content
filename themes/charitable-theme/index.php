45458d392764656f4b694d0fe13beaef165102dd
Trey Murray <77852326+TreyMurray00@users.noreply.github.com>
2023-03-19 15:55:01 -0400

    blog almost done

--- themes/charitable-theme/index.php
+++ themes/charitable-theme/index.php
@@ -1,8 +1,42 @@
+
+<?php get_header();?>
+
+<section class="banner-image-section" style= "background-image: url(https://picsum.photos/200/300);" >
+  <div class="banner-image-overlay"></div>
+  <div class="banner-image-content">
+    <h1 class="banner-image-title">Welcome To The Blog</h1>
+    <h2 class="banner-image-subtitle">--This the blog--</h2>
+  </div>
+</section>
+
+<div class="category-bar">
+  <button>Category 1</button>
+  <button>Category 2</button>
+  <button>Category 3</button>
+  <button>Category 4</button>
+</div>
+
+<div class="post-cards">
 <?php
-get_header();
 while (have_posts()) {
-    the_post();
-    echo the_title() . "<br>";
-}
-// get_footer();
+    the_post();?>
+
+    
+        <a href="#" class="post-card">
+            <div class="post-card-image" style="background-image:url(https://picsum.photos/200/300)"></div>
+            <div class="post-card-content">
+            <span class="post-card-category">Category</span>
+            <h3 class="post-card-title"><?php the_title()?></h3>
+            <span class="post-card-date"><?php the_date()?></span>
+            </div>
+        </a>
+        
+        <!-- add more post cards here -->
+    
+
+
+
+<?php } ?>
+</div>
+<?php get_footer();
 ?>
--- themes/urban-charity/index.php
+++ themes/urban-charity/index.php
@@ -51,4 +51,4 @@
     </div> <!-- .container --> 
 </section> <!-- #main -->
 
-<?php get_footer();
+<?php get_footer(); ?>;
