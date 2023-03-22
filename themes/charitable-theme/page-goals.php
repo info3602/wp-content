98de01c1b0566ff5337b6fb6468ec00147982427
RiannaJ <riannajames@hotmail.com>
2023-03-22 19:56:35 -0400

    Goals Page

---
+++ themes/charitable-theme/page-goals.php
@@ -0,0 +1,52 @@
+<?php get_header() ?>
+
+<!-- Our Goals Start -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+            <h6 class="section-title text-center text-primary text-uppercase">Our Goals</h6>
+            <h6 class="section-title text-start text-primary text-uppercase">
+            Mission Statement
+            </h6>
+            <p class="mb-4">
+                To provide support and assistance to refugees who were forced to flee their countries due 
+                conflict, persecution or other reasons. <br /><br />
+            </p>
+            
+            <div class="col-6 text-start">
+                <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.5s" src=<?php echo get_template_directory_uri() . "/img/team-1.jpg" ?>>
+                <p><span >Jane Doe.</span> Cordinator</p>
+            </div>
+            <div class="col-6 text-center">
+                <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.5s" src=<?php echo get_template_directory_uri() . "/img/team-2.jpg" ?>>
+                <p><span >Jane Doe.</span> Manager</p>
+            </div>
+             <div class="col-6 text-center">
+                <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.5s" src=<?php echo get_template_directory_uri() . "/img/team-3.jpg" ?>>
+                <p><span >John Doe.</span> CEO</p>
+            </div>
+            <div class="col-6 text-end">
+                <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.5s" src=<?php echo get_template_directory_uri() . "/img/team-4.jpg" ?>>
+                <p><span >John Doe.</span> Supervisor</p>
+            </div>
+
+            <h6 class="section-title text-start text-primary text-uppercase">
+            Provide Healthcare
+            </h6>
+            <h6 class="section-title text-center text-primary text-uppercase">
+            Provide Basic Needs
+            </h6>
+            <h6 class="section-title text-end text-primary text-uppercase">
+            Provide Legal Assistance
+            </h6>
+            <h6 class="section-title text-center text-primary text-uppercase">
+            Provide Education
+            </h6>
+            <h6 class="section-title text-center text-primary text-uppercase">
+            Provide Mental Health Support
+            </h6>
+        </div>
+    </div>
+</div>
+
+<?php get_footer() ?>
