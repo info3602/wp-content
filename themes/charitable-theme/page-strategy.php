9ded2f1d0e613b9f8cfee7c693549dea51b8e679
RiannaJ <riannajames@hotmail.com>
2023-03-22 19:56:48 -0400

    Strategy Page

---
+++ themes/charitable-theme/page-strategy.php
@@ -0,0 +1,60 @@
+<?php get_header() ?>
+
+<!-- Our Goals Start -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+            <h6 class="section-title text-center text-primary text-uppercase">Our Strategy</h6>
+            <h6 class="section-title text-start text-primary text-uppercase">
+            Programs and Services
+            </h6>
+            <p class="mb-4">
+                We, at Chariiable, provide a variety of programs and services to refugees. <br /><br />
+            </p>
+        </div>
+
+          <div class="container-xxl py-5">
+            <div class="container">
+                <div class="wow fadeInUp" data-wow-delay="0.1s">
+                <h1 class="mb-5">Chatiable Services</h1>
+                </div>
+                <div class="row g-4">
+
+                <div class="col-lg-6">
+                    <div class="row g-3">
+                    <div class="col-6 text-end">
+                        <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.5s" src=<?php echo get_template_directory_uri() . "/img/carousel-3.jpg" ?>>
+                    </div>
+                    <div class="col-6 text-start">
+                        <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.5s" src=<?php echo get_template_directory_uri() . "/img/carousel-4.jpg" ?>>
+                    </div>
+                    <div class="col-6 text-end">
+                        <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.5s" src=<?php echo get_template_directory_uri() . "/img/carousel-5.jpg" ?>>
+                    </div>
+
+            <div class="container-xxl py-5">
+            <div class="container">
+                <div class="wow fadeInUp" data-wow-delay="0.1s">
+                <h1 class="mb-5">Chatiable Programs</h1>
+                </div>
+                <div class="row g-4">
+
+                <div class="col-lg-6">
+                    <div class="row g-3">
+                    <div class="col-6 text-end">
+                        <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.5s" src=<?php echo get_template_directory_uri() . "/img/carousel-3.jpg" ?>>
+                    </div>
+                    <div class="col-6 text-start">
+                        <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.5s" src=<?php echo get_template_directory_uri() . "/img/carousel-4.jpg" ?>>
+                    </div>
+                    <div class="col-6 text-end">
+                        <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.5s" src=<?php echo get_template_directory_uri() . "/img/carousel-5.jpg" ?>>
+                    </div>
+
+    </div>
+</div>
+
+
+
+
+<?php get_footer() ?>
