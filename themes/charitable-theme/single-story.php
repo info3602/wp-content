5a98534c94381affecb7c819a19d35aa5c589145
RiannaJ <riannajames@hotmail.com>
2023-03-23 11:04:56 -0400

    Create single-story.php

---
+++ themes/charitable-theme/single-story.php
@@ -0,0 +1,54 @@
+<?php get_header() ?>
+
+<!-- Page Header Start -->
+<div class="container-fluid page-header mb-5 p-0" style="background-image: url(img/carousel-1.jpg);">
+            <div class="container-fluid page-header-inner py-5">
+                <div class="container text-center pb-5">
+                    <h1 class="display-3 text-white mb-3 animated slideInDown">Impact Stories</h1>
+                    
+                </div>
+            </div>
+        </div>
+        <!-- Page Header End -->
+
+        <!-- Testimonial Start -->
+        <div class="container-xxl testimonial my-5 py-5 bg-dark wow zoomIn" data-wow-delay="0.1s">
+            <div class="container">
+                <div class="owl-carousel testimonial-carousel py-5">
+                    <div class="testimonial-item position-relative bg-white rounded overflow-hidden">
+                        <p>After her home was badly damaged in the earthquakes, Irmine and her family – one of whom has a chronic illness – took shelter at an exhibition center in Sanliurfa. They are among 9,000 people who are receiving basic necessities like food, water, hygiene supplies, and heating in this venue.</p>
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid flex-shrink-0 rounded" src="img/testimonial-1.jpg" style="width: 45px; height: 45px;">
+                            <div class="ps-3">
+                                <h6 class="fw-bold mb-1">Irmine's Story</h6>
+                            </div>
+                        </div>
+                        <i class="fa fa-quote-right fa-3x text-primary position-absolute end-0 bottom-0 me-4 mb-n1"></i>
+                    </div>
+                    <div class="testimonial-item position-relative bg-white rounded overflow-hidden">
+                        <p>In the parking lot of a small shopping center in the city of Adiyaman, an informal tent encampment has been set up. Families are now taking shelter here, after Adiyaman, a bustling city of around 600,000, was devastated by the quakes. Many buildings are uninhabitable, while dozens more have collapsed entirely.
+                            Among those who have found safety in this parking lot is Mehmet and his extended family of 20 – the oldest being 70, the youngest just five. </p>
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid flex-shrink-0 rounded" src="img/testimonial-2.jpg" style="width: 45px; height: 45px;">
+                            <div class="ps-3">
+                                <h6 class="fw-bold mb-1">Mehmet's Story</h6>
+                            </div>
+                        </div>
+                        <i class="fa fa-quote-right fa-3x text-primary position-absolute end-0 bottom-0 me-4 mb-n1"></i>
+                    </div>
+                    <div class="testimonial-item position-relative bg-white rounded overflow-hidden">
+                        <p>We met Burak and his parents sitting by a fire outside a completely destroyed building in Adiyaman, Turkey, that was previously their family home. Once eight floors tall, the building is now just one storey high, having collapsed to a pile of rubble following the earthquake. The family were buried beneath the wreckage, but miraculously managed to escape. Burak, a mariner by profession, has now been living on the street for five days and nights, facing brutally cold temperatures</p>
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid flex-shrink-0 rounded" src="img/testimonial-3.jpg" style="width: 45px; height: 45px;">
+                            <div class="ps-3">
+                                <h6 class="fw-bold mb-1">Burak's Story</h6>
+                            </div>
+                        </div>
+                        <i class="fa fa-quote-right fa-3x text-primary position-absolute end-0 bottom-0 me-4 mb-n1"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Testimonial End -->
+
+<?php get_footer() ?>
