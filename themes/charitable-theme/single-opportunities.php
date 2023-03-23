0afde1703603894d291d7458a1113e2308eebb66
RiannaJ <riannajames@hotmail.com>
2023-03-23 12:18:52 -0400

    Create single-opportunities.php

---
+++ themes/charitable-theme/single-opportunities.php
@@ -0,0 +1,134 @@
+<?php get_header() ?>
+
+<!-- Page Header Start -->
+<div class="container-fluid page-header mb-5 p-0" style="background-image: url(img/carousel-1.jpg);">
+            <div class="container-fluid page-header-inner py-5">
+                <div class="container text-center pb-5">
+                    <h1 class="display-3 text-white mb-3 animated slideInDown">Volunteer Opportunities</h1>
+                    </div>
+            </div>
+        </div>
+        <!-- Page Header End -->
+
+        <!-- About Opportunities Start -->
+            <div class="container-xxl py-5">
+                <div class="container">
+                    <div class="row g-5 align-items-center">
+                         <div class="col-lg-6">
+                            
+                            <h1 class="mb-4">
+                            How You Can Help <span class="text-primary text-uppercase">Saves Lives</span>
+                            </h1>
+                            <p class="mb-4">
+                                As a non profit organisation we rely on the support of the community to carry out our 
+                                mission. There are many ways you can help us make a difference. <br /><br />
+                                We welcome anyone who is interested in helping us make a difference. Whether you are 
+                                an individual, a business or an organisation, there are many ways you can get involved
+                                with our non-profit charity organisation for refugees.
+                                <br /><br />
+                            </p>
+                         </div>
+                            <div class="col-lg-6">
+                                    <div class="row g-3">
+                                        <div class="col-6 text-end">
+                                            <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.1s" src="img/about-1.jpg" style="margin-top: 25%;">
+                                        </div>
+                                        <div class="col-6 text-start">
+                                            <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.3s" src="img/about-2.jpg">
+                                        </div>
+                                        <div class="col-6 text-end">
+                                            <img class="img-fluid rounded w-50 wow zoomIn" data-wow-delay="0.5s" src="img/about-3.jpg">
+                                        </div>
+                                        <div class="col-6 text-start">
+                                            <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.7s" src="img/about-4.jpg">
+                                        </div>
+                                    </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+            <!-- About Opportunities End -->
+
+            <!-- Steps Start -->
+            <div class="container-xxl py-5">
+                <div class="container">
+                    <div class="row g-5 align-items-center">
+                        <div class="col-lg-6">
+                        
+                            <h1 class="mb-4">
+                            Here are some steps to
+                            <span class="text-primary text-uppercase">Volunteer</span>
+                            </h1>
+                            <p class="mb-4">
+                                Here are some easy steps to
+                                <span class="text-primary text-uppercase">Volunteer</span>
+                                <br />
+                                <i class="fa fa-solid fa-arrow-right text-primary mb-2"></i>
+                                <h2>Step 1</h2><br />
+                                Go to our site and select the Volunteer Opportunities option.
+                                <br /><br />
+                                <i class="fa fa-solid fa-arrow-right text-primary mb-2"></i>
+                                <h2>Step 2</h2><br />
+                                Fill out information required on form and click submit once completed.
+                                <br /><br />
+                                <i class="fa fa-solid fa-arrow-right text-primary mb-2"></i>
+                                <h2> Step 3</h2><br />
+                                Look our for confirmation email and further instructions.
+                                <br /><br />
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Steps End -->
+
+        <!-- Volunteer Testimonial Start -->
+        <div class="container-xxl py-5">
+            <div class="container">
+                <div class="row g-4">
+                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                        <div class="room-item shadow rounded overflow-hidden">
+                            <div class="position-relative">
+                                <img class="img-fluid" src="img/testimonial-1.jpg" alt="">
+                            </div>
+                            <div class="p-4 mt-2">
+                                <div class="d-flex justify-content-between mb-3">
+                                    <h5 class="mb-0">Jane Doe</h5>
+                                </div>
+                                <p class="text-body mb-3">Being involved in this charity is the most rewarding experience of my life</p>      
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                        <div class="room-item shadow rounded overflow-hidden">
+                            <div class="position-relative">
+                                <img class="img-fluid" src="img/testimonial-1.jpg" alt="">
+                            </div>
+                            <div class="p-4 mt-2">
+                                <div class="d-flex justify-content-between mb-3">
+                                    <h5 class="mb-0">Sam James</h5>
+                                </div>
+                                <p class="text-body mb-3">I feel a sense of pride when I volunteer my time for this cause</p>      
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                        <div class="room-item shadow rounded overflow-hidden">
+                            <div class="position-relative">
+                                <img class="img-fluid" src="img/testimonial-1.jpg" alt="">
+                            </div>
+                            <div class="p-4 mt-2">
+                                <div class="d-flex justify-content-between mb-3">
+                                    <h5 class="mb-0">Larry John</h5>
+                                </div>
+                                <p class="text-body mb-3">Hearing the impact stories is so inspiring to me knowing that I helped made that happen</p>      
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Volunteer Testimonial End -->
+
+
+<?php get_footer() ?>
