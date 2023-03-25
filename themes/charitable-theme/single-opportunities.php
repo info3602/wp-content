6b99a5b307d76b6852f7264de7d48d669377f99f
RiannaJ <riannajames@hotmail.com>
2023-03-25 09:24:17 -0400

    Updated single pages

--- themes/charitable-theme/single-opportunities.php
+++ themes/charitable-theme/single-opportunities.php
@@ -1,130 +1,77 @@
 <?php get_header() ?>
 
 <!-- Page Header Start -->
-<div class="container-fluid page-header mb-5 p-0" style="background-image: url(img/carousel-1.jpg);">
+<div class="container-fluid page-header mb-5 p-0" 
+    style="background-image: url(<?php echo get_theme_file_uri("img/carousel-4.jpg") ?>);">
             <div class="container-fluid page-header-inner py-5">
                 <div class="container text-center pb-5">
-                    <h1 class="display-3 text-white mb-3 animated slideInDown">Volunteer Opportunities</h1>
-                    </div>
+                    <h1 class="display-3 text-white mb-3 animated slideInDown">
+                    <?php the_title(); ?>
+                    </h1>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb justify-content-center text-uppercase">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item"><a href="#">Donate</a></li>
+                            <li class="breadcrumb-item text-white active" aria-current="page">About</li>
+                        </ol>
+                    </nav>
+                </div>
             </div>
         </div>
         <!-- Page Header End -->
 
-        <!-- About Opportunities Start -->
-            <div class="container-xxl py-5">
-                <div class="container">
-                    <div class="row g-5 align-items-center">
-                         <div class="col-lg-6">
-                            
-                            <h1 class="mb-4">
-                            How You Can Help <span class="text-primary text-uppercase">Saves Lives</span>
-                            </h1>
-                            <p class="mb-4">
-                                test
-                                <br /><br />
-                            </p>
-                         </div>
-                            <div class="col-lg-6">
-                                    <div class="row g-3">
-                                        <div class="col-6 text-end">
-                                            <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.1s" src="img/about-1.jpg" style="margin-top: 25%;">
-                                        </div>
-                                        <div class="col-6 text-start">
-                                            <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.3s" src="img/about-2.jpg">
-                                        </div>
-                                        <div class="col-6 text-end">
-                                            <img class="img-fluid rounded w-50 wow zoomIn" data-wow-delay="0.5s" src="img/about-3.jpg">
-                                        </div>
-                                        <div class="col-6 text-start">
-                                            <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.7s" src="img/about-4.jpg">
-                                        </div>
-                                    </div>
-                            </div>
-                    </div>
-                </div>
-            </div>
-            <!-- About Opportunities End -->
-
-            <!-- Steps Start -->
-            <div class="container-xxl py-5">
-                <div class="container">
-                    <div class="row g-5 align-items-center">
-                        <div class="col-lg-6">
-                        
-                            <h1 class="mb-4">
-                            Here are some steps to
-                            <span class="text-primary text-uppercase">Volunteer</span>
-                            </h1>
-                            <p class="mb-4">
-                                Here are some easy steps to
-                                <span class="text-primary text-uppercase">Volunteer</span>
-                                <br />
-                                <i class="fa fa-solid fa-arrow-right text-primary mb-2"></i>
-                                <h2>Step 1</h2><br />
-                                Go to our site and select the Volunteer Opportunities option.
-                                <br /><br />
-                                <i class="fa fa-solid fa-arrow-right text-primary mb-2"></i>
-                                <h2>Step 2</h2><br />
-                                Fill out information required on form and click submit once completed.
-                                <br /><br />
-                                <i class="fa fa-solid fa-arrow-right text-primary mb-2"></i>
-                                <h2> Step 3</h2><br />
-                                Look our for confirmation email and further instructions.
-                                <br /><br />
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- Steps End -->
-
-        <!-- Volunteer Testimonial Start -->
+        <!-- Steps Start -->
         <div class="container-xxl py-5">
-            <div class="container">
-                <div class="row g-4">
-                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="room-item shadow rounded overflow-hidden">
-                            <div class="position-relative">
-                                <img class="img-fluid" src="img/testimonial-1.jpg" alt="">
-                            </div>
-                            <div class="p-4 mt-2">
-                                <div class="d-flex justify-content-between mb-3">
-                                    <h5 class="mb-0">Jane Doe</h5>
-                                </div>
-                                <p class="text-body mb-3">Being involved in this charity is the most rewarding experience of my life</p>      
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="room-item shadow rounded overflow-hidden">
-                            <div class="position-relative">
-                                <img class="img-fluid" src="img/testimonial-1.jpg" alt="">
-                            </div>
-                            <div class="p-4 mt-2">
-                                <div class="d-flex justify-content-between mb-3">
-                                    <h5 class="mb-0">Sam James</h5>
-                                </div>
-                                <p class="text-body mb-3">I feel a sense of pride when I volunteer my time for this cause</p>      
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="room-item shadow rounded overflow-hidden">
-                            <div class="position-relative">
-                                <img class="img-fluid" src="img/testimonial-1.jpg" alt="">
-                            </div>
-                            <div class="p-4 mt-2">
-                                <div class="d-flex justify-content-between mb-3">
-                                    <h5 class="mb-0">Larry John</h5>
-                                </div>
-                                <p class="text-body mb-3">Hearing the impact stories is so inspiring to me knowing that I helped made that happen</p>      
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div class="container">
+            <div class="row g-5 align-items-center">
+            <div class="col-lg-6">
+                
+                <h1 class="mb-4">
+                How you can help 
+                <span class="text-primary text-uppercase">Save Lives</span>
+                </h1>
+                <p class="mb-4">
+                Here are some easy steps to
+                    <span class="text-primary text-uppercase">Volunteer</span>
+                    <br />
+                    
+                    <h5>Step 1</h5><br />
+                    Go to our site and select the Volunteer Opportunities option.
+                    <br /><br />
+                    
+                    <h5>Step 2</h5><br />
+                    Fill out information required on form and click submit once completed.
+                    <br /><br />
+                    
+                    <h5> Step 3</h5><br />
+                    Look our for confirmation email and further instructions.
+                    <br /><br />
+                </p>
+            </div>
             </div>
         </div>
-        <!-- Volunteer Testimonial End -->
+        </div>
+        <!-- Steps End -->
 
+        <div class="container">
+          <img src=<?php echo get_theme_file_uri("img/testimonial-1.jpg") ?> alt="Avatar" style="width:90px">
+          <p>
+            <span>Jane Doe</span><br></br> CEO at Mighty Schools.
+          </p>
+          <p>It is very rewarding to be apart of this charity.</p>
+        </div>
+
+        <div class="container">
+          <img src=<?php echo get_theme_file_uri("img/testimonial-2.jpg") ?> alt="Avatar" style="width:90px">
+          <p>
+            <span>Chris Fox.</span><br></br> 
+          </p>
+          <p>It's amazing to see the impact we have on people's lives</p>
+        </div>
+
+
+
+        
+       
 
 <?php get_footer() ?>
--- themes/charitable-theme/single-story.php
+++ themes/charitable-theme/single-story.php
@@ -2,7 +2,7 @@
 
 <!-- Page Header Start -->
 <div class="container-fluid page-header mb-5 p-0" 
-    style="background-image: url(<?php echo get_theme_file_uri("img/carousel-3.jpg") ?>);">
+    style="background-image: url(<?php echo get_theme_file_uri("img/carousel-4.jpg") ?>);">
             <div class="container-fluid page-header-inner py-5">
                 <div class="container text-center pb-5">
                     <h1 class="display-3 text-white mb-3 animated slideInDown">
@@ -20,52 +20,56 @@
         </div>
         <!-- Page Header End -->
 
-        <!-- About Start -->
+<!-- About Start -->
 <div class="container-xxl py-5">
   <div class="container">
     <div class="row g-5 align-items-center">
-
-    <div class="col-lg-12">
-        <p class="mb-10">
+      <div class="col-lg-6">
+        
+        
+        <p class="mb-4">
         <?php
-            while(have_posts()){ 
-                the_post(); 
-                ?>
-                <?php the_content(); ?>
-                
-                <?php
-                }
-                ?>
-        </p>
-
+                while(have_posts()){ 
+                    the_post(); 
+                    ?>
+                    <?php the_content(); ?>
+                    
+                    <?php
+                    }
+            ?>
+        </p> 
       </div>
-      
-      <div class="col-lg-4">
-        <div class="row g-5">
+      <div class="col-lg-6">
+        <div class="row g-3">
           <div class="col-6 text-end">
-            <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.5s" src=<?php echo get_template_directory_uri() . "/img/carousel-4.jpg" ?>>
+            <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.5s" src=<?php echo get_template_directory_uri() . "/img/carousel-3.jpg" ?>>
           </div>
-          <div class="col-6 text-end">
-            <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.5s" src=<?php echo get_template_directory_uri() . "/img/carousel-5.jpg" ?>>
+          <div class="col-6 text-start">
+            <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.5s" src=<?php echo get_template_directory_uri() . "/img/carousel-4.jpg" ?>>
           </div>
           <div class="col-6 text-end">
             <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.5s" src=<?php echo get_template_directory_uri() . "/img/carousel-3.jpg" ?>>
-          </div><div class="col-6 text-end">
-            <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.5s" src=<?php echo get_template_directory_uri() . "/img/carousel-4.jpg" ?>>
           </div>
-          
-          
+          <br></br>
+          <br></br>
+          <div class="col-12 text-center">
+          <a class="btn btn-primary py-3 px-5 mt-2" href=<?php echo site_url("/home"); ?>>More Related Stories</a>
+          </div>
         </div>
+        
       </div>
     </div>
   </div>
 </div>
 <!-- About End -->
 
-<!-- Video Start -->
-<iframe width="560" height="315" src="https://www.youtube.com/embed/EXJKaQF0Rzw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>     
-        <!-- Video Start -->
+
+
+
 
         
+
+
+
 
 <?php get_footer() ?>
